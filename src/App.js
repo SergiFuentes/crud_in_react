c9efcaa4cbf44ee8750bd82fd56a7f1745baff09
SergiFuentes <you@example.com>
2021-12-29 20:38:39 +0100

    starting CrudApi, creating helpers folder and helpHttp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { CrudApi } from "./components/CrudApi";
 import { CrudApp } from "./components/CrudApp";
 
 function App() {
   return (
     <div className="App">
       <h1>REACT exercices</h1>
+      <CrudApi/> 
+      <hr/>
       <CrudApp />
       <hr/>
     </div>
