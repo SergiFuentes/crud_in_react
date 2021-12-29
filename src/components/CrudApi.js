02b2d2144c5352c43f60476ac92945664e8d9b88
SergiFuentes <you@example.com>
2021-12-30 00:48:28 +0100

    adding loader and message components

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -1,49 +1,85 @@
-import React, { useState } from "react";
-import { CrudTable } from "./CrudTable";
+import React, { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import { CrudForm } from "./CrudForm";
+import { CrudTable } from "./CrudTable";
+import { Loader } from "./Loader";
+import { Messaje } from "./Messaje";
 
 export const CrudApi = () => {
-  const [db, setDb] = useState([]);
-  const [dataToEdit, setDataToEdit] = useState(null);
+    const [db, setDb] = useState(null);
+    const [ dataToEdit, setDataToEdit ] = useState( null );
 
-  const createData = (data) => {
-    data.id = Date.now();
-    //console.log( data );
-    setDb([...db, data]);
-  };
+    const [ error, setError ] = useState( null );
+    const [ loading , setLoading ] = useState( false );
+        
+    let api = helpHttp();
+    let url = "http://localhost:5000/user";
+    
+    useEffect( () =>
+    {
+        setLoading( true );
+        api.get( url ).then( ( res ) =>
+        { //console.log( res ) } );
+            if ( !res.err )
+            {
+                setDb( res );
+                setError( null );
+            } else
+            {
+                setDb( null );
+                setError( res );
 
-  const updateData = (data) => {
-    let newData = db.map((el) => (el.id === data.id ? data : el));
-    setDb(newData);
-  };
+            }
+            setLoading( false );
+        } );
+    },[] );
+  
+    const createData = ( data ) =>
+    {
+        data.id = Date.now();
+        //console.log( data );
+        setDb( [ ...db, data ] );
+    };
 
-  const deleteData = (id) => {
-    let isDelete = window.confirm(
-      `¿Are you sure to delete the register with id '${id}'`
+    const updateData = ( data ) =>
+    {
+        let newData = db.map( ( el ) => ( el.id === data.id ? data : el ) );
+        setDb( newData );
+    };
+
+    const deleteData = ( id ) =>
+    {
+        let isDelete = window.confirm(
+            `¿Are you sure to delete the register with id '${ id }'`
+        );
+
+        if ( isDelete )
+        {
+            let newData = db.filter( ( el ) => el.id !== id );
+            setDb( newData );
+        }
+    };
+
+    return (
+        <div>
+            <h2>CRUD Api</h2>
+            <article className="grid-1-2">
+                <CrudForm
+                    createData={ createData }
+                    updateData={ updateData }
+                    dataToEdit={ dataToEdit }
+                    setDataToEdit={ setDataToEdit }
+                />
+                { loading && <Loader /> }
+                {error && <Messaje/> }
+                { db && <CrudTable
+                    data={ db }
+                    setDataToEdit={ setDataToEdit }
+                    deleteData={ deleteData }
+                /> }
+                
+                
+            </article>
+        </div>
     );
-
-    if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
-    }
-  };
-
-  return (
-    <div>
-      <h2>CRUD Api</h2>
-      <article className="grid-1-2">
-        <CrudForm
-          createData={createData}
-          updateData={updateData}
-          dataToEdit={dataToEdit}
-          setDataToEdit={setDataToEdit}
-        />
-        <CrudTable
-          data={db}
-          setDataToEdit={setDataToEdit}
-          deleteData={deleteData}
-        />
-      </article>
-    </div>
-  );
 };
---
+++ src/components/Messaje.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Messaje = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
